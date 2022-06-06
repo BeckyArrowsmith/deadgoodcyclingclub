f7daa5fecbc7c115f8911c5e8a5a32e47c185d25
BeckyArrowsmith <beckyarrowsmith@googlemail.com>
2022-06-06 16:43:20 +0100

    Add ChakraProvider

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
+import { ChakraProvider } from '@chakra-ui/react'
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
@@ -13,7 +14,9 @@ const root = ReactDOM.createRoot(container)
 root.render(
   <React.StrictMode>
     <ColorModeScript />
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
 

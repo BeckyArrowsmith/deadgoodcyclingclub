bf06ef59417053a2c6d2450aa702a6483ff3b96c
BeckyArrowsmith <beckyarrowsmith@googlemail.com>
2022-06-08 20:49:40 +0100

    Refactor nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import * as React from "react"
 import {
   Flex,
-  Heading,
   VStack,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
 import Nav from './components/Nav';
 
 export const App = () => (
-  // <ChakraProvider theme={theme}>
-  //   <Nav/>
-  // </ChakraProvider>
-  <VStack p={5} backgroundColor="green">
+  <VStack p={5} bg="purple.default">
     <Flex w="100%">
       <Nav/>
     </Flex>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,25 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  GridItem,
   theme,
   Heading
 } from "@chakra-ui/react"
 
-export class Nav extends Component {
-  render() {
-    return(
-      <ChakraProvider theme={theme}>
-        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">Dead Good Cycling Club</Heading>
-      </ChakraProvider>      
-      )
-  }
+function Nav() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Heading ml="8" size="md" fontWeight="semibold" color="cyan.default">Dead Good Cycling Club</Heading>
+    </ChakraProvider>  
+  )
 }
 
 export default Nav;
